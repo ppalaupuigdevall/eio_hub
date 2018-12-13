c63b52e8a262b3a2bd755c8ef768b7505fc43e4a
Ponç Palau Puigdevall <ppalaupuigdevall@gmail.com>
2018-12-13 03:08:36 +0100

    Add files via upload

---
+++ receiving_burst.ino
@@ -0,0 +1,132 @@
+#include <IRremote.h>
+#include <IRremoteInt.h>
+/*TODO:
+    Hay que hacer que el receiver haga un check de las init sequence y compruebe crc
+*/
+
+#define SEQ_LEN_NO_CRC 14
+#define SEQ_LEN_CRC 16 //4 Bursts*4 Bytes
+int RECV_PIN = 5;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup() { 
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+
+/*A burst is a 4 Bytes sequence*/
+typedef union{
+  byte myByte[4];
+  unsigned long myLong;
+}burst;
+
+typedef union {
+ float floatingPoint;
+ byte binary[4];
+}binaryFloat;
+
+typedef union{
+  unsigned int myInt;
+  byte byte_array[2];
+}uint2bytes;
+
+typedef union {
+  int myInt;
+  byte byte_array[2];
+}int2bytes;
+
+byte compute_CRC(byte b){
+    byte count = 0;
+    while(b != 0){
+      if(b%2 ==1){
+        count++;
+      }
+      b /= 2;
+    }
+    return count;
+}
+
+byte compute_array_CRC(byte *arr){
+  byte count = 0;
+  byte len = sizeof(arr);
+  Serial.println(len, DEC);
+  for(byte i = 0; i <SEQ_LEN_NO_CRC; i++){
+    count = count + compute_CRC(arr[i]);
+  }
+  return count;
+}
+  
+void loop() {
+  // put your main code here, to run repeatedly:
+  byte sequence[SEQ_LEN_CRC];
+  byte sequence_to_compute_crc[SEQ_LEN_CRC];
+  byte crc;
+  bool end_of_seq = false;
+  bool incorrect_crc = false;
+  byte j = 0;
+  while(!end_of_seq || !incorrect_crc){
+   
+    if(irrecv.decode(&results)){
+      byte first_bytes_rcv[4];
+      unsigned long valor_rebut = results.value;
+      burst rcv_burst;
+      rcv_burst.myLong = valor_rebut;
+      
+      first_bytes_rcv[3] = rcv_burst.myByte[0]; //initseq0
+      first_bytes_rcv[2] = rcv_burst.myByte[1]; //initseq1
+      first_bytes_rcv[1] = rcv_burst.myByte[2]; //crc
+      first_bytes_rcv[0] = rcv_burst.myByte[3]; //padding
+      Serial.println(first_bytes_rcv[0], HEX);
+      Serial.println(first_bytes_rcv[1], HEX);
+      Serial.println(first_bytes_rcv[2], HEX);
+      Serial.println(first_bytes_rcv[3], HEX);
+      for(byte i=0; i<4; i++){
+        sequence[i+j] = first_bytes_rcv[i];
+      }
+  
+      if(sequence[0] == 0xa0 && sequence[1] == 0xa0){
+        Serial.println("Rebo del node A");
+        //Update the sequence index
+        j = j + 4;
+      }else if(sequence[0] == 0xb0 && sequence[1] == 0xb0){
+        Serial.println("Rebo del node B");
+        //Update the sequence index
+        j = j + 4;
+      }else{
+        //The received signal is nothing known by the receiver
+        end_of_seq = true;
+      }
+      
+      if(j == 16){
+        //Aixo vol dir que he rebut tota la sequencia (15 Bytes), ara ja puc fer els calculs que toquin: 1 - calcular crc i en cas que sigui OK treure info de sensors i enviar-la a josep
+        Serial.println("He rebut 4 paquets!!");
+        for(byte k = 0; k<2; k++){
+          sequence_to_compute_crc[k] = sequence[k];
+        }
+        
+        for(byte k = 2; k < SEQ_LEN_NO_CRC - 2; k++){
+          sequence_to_compute_crc[k] = sequence[k + 2];
+        }
+        byte computed_crc = compute_array_CRC(sequence_to_compute_crc);
+        Serial.println(crc, HEX);
+        Serial.println(computed_crc, HEX);
+        if(computed_crc == crc){
+          Serial.println("CRC OK!, treure info i enviar a josep");
+        }else{
+          Serial.println("CRC NOT OK! (-_-)");
+        }
+        //Passi el que passi a dalt hem de sortir del while i seguir llegint
+        end_of_seq = true;
+        
+      }
+      
+   //Receive next packet
+    irrecv.resume();
+    delay(10);
+    }
+     
+  }
+delay(10);
+}
---
+++ send_just_3_decs.ino
@@ -0,0 +1,187 @@
+#include <IRremote.h>
+
+//These 14 Bytes are: initSeq (2B) + temp (4B) + press (4B) + humidity (4B) 
+#define SEQ_LEN_NO_CRC 14 // In Bytes
+
+IRsend irsend;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+/*Let us suppose that what we receive from the other groups is a float (4 Bytes) for each measurement.*/
+float readings_from_sensors[3] = {10.41, 109999.2, 89.27};
+
+/*A burst is a 4 Bytes sequence*/
+typedef union{
+  byte myByte[4];
+  unsigned long myLong;
+}burst;
+
+typedef union {
+ float floatingPoint;
+ byte binary[4];
+}binaryFloat;
+
+typedef union{
+  unsigned int myInt;
+  byte byte_array[2];
+}uint2bytes;
+
+typedef union {
+  int myInt;
+  byte byte_array[2];
+}int2bytes;
+
+byte compute_CRC(byte b){
+    byte count = 0;
+    while(b != 0){
+      if(b%2 ==1){
+        count++;
+      }
+      b /= 2;
+    }
+    return count;
+}
+
+byte compute_array_CRC(byte *arr){
+  byte count = 0;
+  byte len = sizeof(arr);
+  Serial.println(len, DEC);
+  for(byte i = 0; i <SEQ_LEN_NO_CRC; i++){
+    count = count + compute_CRC(arr[i]);
+  }
+  return count;
+}
+
+void loop() {
+
+  /*
+    The code from the other groups should be hopefully something like:
+    float readings_from_sensors[3];
+    readings_from_sensors[0] = readTemp();
+    readings_from_sensors[1] = readPress();
+    readings_from_sensors[2] = readHumi();
+  */
+  
+  //This is the sequence over which we'll compute the CRC
+  byte sequence[SEQ_LEN_NO_CRC];
+  byte initseq0 = 0xa0;
+  byte initseq1 = 0xa0;
+  sequence[0] = initseq0;
+  sequence[1] = initseq1;
+  
+//TEMPERATURA
+  /*The Packet structure for the temperature will be: For the node A: [AF, Integer_Part, Decimal_part, Decimal_part]
+                                                      For the node B: [BF, Integer_Part, Decimal_part, Decimal_part]
+  */
+  float tempera = readings_from_sensors[0];
+
+  //TEMPERATURE: fornode A temp_init_seq is 0xaf, for node B is 0xbf
+  byte temp_init_seq = 0xaf;
+  //Doing this we send just positive numbers but in reception we know that we have to substract 20 again
+  float tempera_added_20 = tempera + 20.0;
+  int tempera_int = (int) tempera_added_20;
+  //This byte below will be send as integer_part
+  byte tempera_byte = (byte) tempera_int;
+  float decimal_part = tempera_added_20 - tempera_int;
+  //This int below will be sent as decimal part
+  int decimal_part_int = (int) (decimal_part * 1000); //We get 3 decimals from the number
+  int2bytes converter;
+  converter.myInt = decimal_part_int;
+  burst temp_burst;
+  temp_burst.myByte[3] = temp_init_seq;
+  temp_burst.myByte[2] = tempera_byte;
+  temp_burst.myByte[1] = converter.byte_array[0];
+  temp_burst.myByte[0] = converter.byte_array[1];
+  for(byte i=0; i<4; i++){
+    sequence[i+2] = temp_burst.myByte[i];
+  }
+  delay(100);
+
+//PRESSIO 
+/*The Packet structure for the pressure will be:      For the node A: [A0, ODD_EVEN, Integer_part, Integer_part]
+                                                      For the node B: [B0, ODD_EVEN, Integer_part, Integer_part]
+  */
+  float pressure = readings_from_sensors[1];
+  //For node A
+  byte pressure_init_seq = 0xaa; //Node b = 0xba
+  byte es_par;
+  unsigned int part_entera = (unsigned int) (pressure/2);
+  if((part_entera%2) == 0){
+    //Si estem aqui (PARELL) el receptor no haura de sumar res, nomes multiplicar per 2
+    es_par = 0x00;
+  }else{
+    //Si estem aqui (IMPARELL) el receptor haura de sumar 1 al resultat
+    es_par = 0xff;
+  }
+
+  uint2bytes pressure_2_bytes;
+  pressure_2_bytes.myInt = part_entera; 
+  
+  burst press_bin;
+  press_bin.myByte[3] = pressure_init_seq;
+  press_bin.myByte[2] = es_par;
+  press_bin.myByte[1] = pressure_2_bytes.byte_array[0];
+  press_bin.myByte[0] = pressure_2_bytes.byte_array[1];
+
+  for(byte i=0; i<4; i++){
+    sequence[i + 4 + 2] = press_bin.myByte[i];
+  }
+  delay(100);
+
+//HUMITAT
+/*The Packet structure for the pressure will be:      For the node A: [A5, Integer_part, Decimal_part, Decimal_part]
+                                                      For the node B: [B5, Integer_Part, Decimal_part, Decimal_part]
+  */
+  float humidity = readings_from_sensors[1];
+  //For node A
+  byte humidity_init_seq = 0xa5; //node B = 0xb5
+  int humidity_int = (int) humidity;
+  //This byte below will be send as integer_part (it fits in a byte as it is from 20 to 90)
+  byte humidity_byte = (byte) humidity_int;
+  float humidity_decimal_part = humidity - humidity_int;
+  //This int below will be sent as decimal part
+  int humidity_decimal_part_int = (int) (decimal_part * 1000); //We get 3 decimals from the number
+  int2bytes converter_humidity;
+  converter_humidity.myInt = humidity_decimal_part_int;
+  
+  burst humidity_burst;
+  humidity_burst.myByte[3] = humidity_init_seq;
+  humidity_burst.myByte[2] = humidity_byte;
+  humidity_burst.myByte[1] = converter_humidity.byte_array[0];
+  humidity_burst.myByte[0] = converter_humidity.byte_array[1];
+  for(byte i=0; i<4; i++){
+    sequence[i + 8 + 2] = humidity_burst.myByte[i];
+  }
+  
+  delay(100);
+  
+  //CRC computation [initseq0, initseq1, crc, 0x00]
+  byte crc = compute_array_CRC(sequence);
+  //We create the first burst (32 bits = 4B : initseq0(1B) + initseq1(1B) + crc(1B) + padding(1B)
+  burst first;
+  //[3,2,1,0]
+  first.myByte[3] = initseq0;
+  first.myByte[2] = initseq1;
+  first.myByte[1] = crc;
+  first.myByte[0] = 0x00; //Padding
+  Serial.println(first.myByte[2]);
+  //Send all the sequences
+  Serial.println(first.myLong, HEX);
+  irsend.sendNEC(first.myLong, 32);
+  delay(100);
+  Serial.println(temp_burst.myLong, HEX);
+  irsend.sendNEC(temp_burst.myLong, 32);
+  delay(100);
+  Serial.println(press_bin.myLong, HEX);
+  irsend.sendNEC(press_bin.myLong, 32);
+  delay(100);
+  Serial.println(humidity_burst.myLong, HEX);
+  irsend.sendNEC(humidity_burst.myLong, 32);
+  delay(500);
+
+}
+  
+
